b9c4bfea90e6e58f34f7e2daf7d1d8038539ddea
Maxime <Maxime@172.16.134.10>
2015-11-17 18:32:10 +0100

    j'ai crée EvenementsRedoutes

--- src/modules/EvenementsRedoutes.java
+++ src/modules/EvenementsRedoutes.java
@@ -1,5 +1,21 @@
 package modules;
 
-public class EvenementsRedoutes {
+import bdc.BDCMetriques;
+
+public class EvenementsRedoutes extends Module {
+
+	private static EvenementsRedoutes evenementsredoutes=new EvenementsRedoutes();
+	
+	
+	
+	private EvenementsRedoutes(){
+		
+		this.predecesseurs.add(Metriques.getInstance());
+		this.predecesseurs.add(CriteresDeSecurite.getInstance());
+		
+	}
+	
+	
+	
 
 }
