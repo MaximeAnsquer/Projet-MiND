40c68be6d42f5420f9e2c6301f96ec2ef53c322c
Maxime <Maxime@172.17.2.211>
2015-11-17 11:10:20 +0100

    commit

--- src/modules/EvenementsRedoutes.java
+++ src/modules/EvenementsRedoutes.java
@@ -1,5 +1,19 @@
 package modules;
 
 public class EvenementsRedoutes {
+	
+	private 
 
+	
+	private EvenementsRedoutes(){
+		
+	}
+	
+	private static class EvenementsRedoutesHolder{
+		private final static EvenementsRedoutes instance=new EvenementsRedoutes();
+	}
+	
+	public static EvenementsRedoutes getEvenementsRedoutes(){
+		return EvenementsRedoutesHolder.instance;
+	}
 }
