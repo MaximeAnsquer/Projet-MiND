28753aef15b91046628ac9b536d42510e08bd358
Ali <Ali@172.16.131.201>
2015-11-19 00:38:05 +0100

    Modification de la classe TypologieBiensSupports Par Ali MAXIME : dans ton constructeur, corrige le nom de ma classe

--- src/abstraction/modules/TypologieBiensSupports.java
+++ src/abstraction/modules/TypologieBiensSupports.java
@@ -1,10 +1,44 @@
 package abstraction.modules;
 
-public class TypologieBiensSupports extends Module {
+import java.util.ArrayList;
 
-	public TypologieBiensSupports(String nom) {
+import autres.TypeBiens;
+
+/**
+ * Cette classe représente le tableau crée lorqu'on clique 
+ * sur l'onglet de typologie de biens supports
+ * 
+ * @author Belghiti Ali
+ */
+
+public class TypologieBiensSupports extends Module {
+	// Variable d'instance
+	ArrayList<TypeBiens> tableau ;
+	
+	// 
+	private static TypologieBiensSupports instance = new TypologieBiensSupports();
+
+	public TypologieBiensSupports() {
 		super("Typologie des biens supports");
-		
+		this.tableau=new ArrayList<>();
+		//this.successeurs.add()
+	}
+	
+	// On ajoute une ligne au tableau seulement si tous les champs sont renseignés
+	public void addTypeBienSupport(TypeBiens type){
+		if (!type.isIncomplete()){
+			this.tableau.add(type);
+		}
+	}
+	
+	// Suppression d'une ligne du tableau
+	public void deleteTypeBienSupport(TypeBiens type){
+		this.tableau.remove(type);
+	}
+	
+	// On récupère notre module
+	public static TypologieBiensSupports getInstance(){
+		return instance;
 	}
 
 }
