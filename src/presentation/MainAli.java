1d1f5d21c770a898cf63e9677354644275039bd6
Ali <Ali@Ali-HP>
2015-12-10 16:49:37 +0100

    François tu m'énerves : voilà le main !!

---
+++ src/presentation/MainAli.java
@@ -0,0 +1,46 @@
+package presentation;
+
+import javax.swing.JOptionPane;
+
+import abstraction.Etude;
+import abstraction.modules.ScenariosDeMenacesGeneriques;
+import abstraction.modules.TypologieDesBiensSupports;
+
+public class MainAli {
+	
+	/**
+	 * Classe destinée à tester les modules
+	 * @author Belghiti Ali
+	 *
+	 */
+	
+	public static Etude etude ;
+	
+	public static void main(String[] args) {
+		
+		// On crée l'étude correspondante
+		etude = new Etude();
+		// Problèmes de nullPointerException
+		
+		//etude.addModule(new TypologieDesBiensSupports());
+		//etude.addModule(new ScenariosDeMenacesGeneriques());
+		
+		String module1 = "Typologie des biens supports";
+		String module2 = "Scénario de menaces génériques";
+		String module3 = "Scenario de menaces typés" ;
+				
+		String[] choix = {module1,module2,module3};
+		String reponse = (String) JOptionPane.showInputDialog(null,
+				"Quelle module voulez-vous ouvrir ?", "Choix fenetre",
+				JOptionPane.QUESTION_MESSAGE, null, choix, choix[0]);
+		
+		if (reponse!= choix[0] && reponse!= choix[1] && reponse!= choix[2] ){
+			reponse = (String) JOptionPane.showInputDialog(null,
+					"Quelle module voulez-vous ouvrir ?", "Choix fenetre",
+					JOptionPane.QUESTION_MESSAGE, null, choix, choix[0]);
+		}
+		if (reponse==choix[0]){
+			FenetreTypologieBiensSupports fen = new FenetreTypologieBiensSupports();
+		}
+	}
+}
