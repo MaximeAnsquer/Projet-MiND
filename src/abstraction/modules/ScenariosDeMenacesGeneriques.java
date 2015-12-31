877c32636eb12d0c27261670e9f1d2ef97f80f93
Ali <Ali@Ali-HP>
2015-12-31 02:47:29 +0100

    J'ai enlevé des import inutiles

--- src/abstraction/modules/ScenariosDeMenacesGeneriques.java
+++ src/abstraction/modules/ScenariosDeMenacesGeneriques.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
-import javax.swing.JLabel;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
