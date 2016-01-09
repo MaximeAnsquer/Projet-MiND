196f1aa87bbbec1b25418688fb9d3209dad61c36
Maxime <Maxime@192.168.1.11>
2016-01-09 22:49:32 +0100

    modifs qui font que analysedesrisques peut etre dispo

---
+++ src/abstraction/modules/Module.java
@@ -0,0 +1,120 @@
+package abstraction.modules;
+import java.util.ArrayList;
+import java.util.Observable;
+
+import javax.swing.JLabel;
+
+import abstraction.Etude;
+
+/** 
+ * Classe visant ï¿½ factoriser tout ce qui est commun aux diffï¿½rents modules.
+ * @author Maxime Ansquer 
+ */
+
+public class Module extends Observable implements IModule  {
+
+	//---Variables d'instance---
+
+	protected ArrayList<Module> predecesseurs; 
+	protected ArrayList<Module> successeurs; 
+	protected String nom;
+	protected boolean cree;
+	protected boolean coherent;
+	protected boolean disponible;
+	protected Etude etude;
+	protected ArrayList<String> problemesDeCoherence;
+
+	//---Constructeurs---
+
+	public Module(String nom) {
+		this.predecesseurs=new ArrayList<Module>();
+		this.successeurs=new ArrayList<Module>();
+		this.nom=nom;
+		this.cree = false;
+		this.problemesDeCoherence = new ArrayList<String>();
+	}
+
+	//---Getters et setters---
+
+	public boolean estCoherent() {
+		return this.coherent;
+	}
+
+	public boolean estCree() {
+		return this.cree;
+	}
+
+	public void setDisponible(){
+		this.disponible=true;
+	}
+
+	public void setCree(boolean cree){
+		this.cree=cree;
+	}
+
+	public void setCoherent(){
+		this.coherent=true;
+	}
+
+	public boolean estDisponible(){
+		
+		System.out.println(" \n ------------------- \n");
+		
+		System.out.println("Vérification de la disponibilité de " + this + "...");
+		boolean resultat = true;
+		System.out.println("predecesseurs : " + this.getPredecesseurs() );
+		
+		if(this.getNom()=="AnalyseDesRisques"){
+			resultat=this.getEtude().getModule("EvenementsRedoutes").estCoherent()&&this.getEtude().getModule("EvenementsRedoutes").estCree();
+		    resultat=resultat&&this.getEtude().getModule("MappingDesBiens").estCoherent()&&this.getEtude().getModule("MappingDesBiens").estCree();
+		    resultat=resultat&&this.getEtude().getModule("ScenariosDeMenacesTypes").estCoherent()&&this.getEtude().getModule("ScenariosDeMenacesTypes").estCree();
+		
+		}
+		
+		else{
+		for(Module m : this.getPredecesseurs()){
+			if(m.estCree()){
+				System.out.println("Le predecesseur " + m + " est cree");
+			}
+			else{
+				System.out.println("Le predecesseur " + m + " n'est pas cree");
+			}
+			if(m.estCoherent()){
+				System.out.println("Le predecesseur " + m + " est coherent");
+			}
+			else{
+				System.out.println("Le predecesseur " + m + " n'est pas coherent");
+			}
+			resultat = resultat && m.estCree() && m.estCoherent();
+		}}
+		this.disponible = resultat;
+		return resultat;
+	}
+
+	public String getNom() {
+		return this.nom;
+	} 
+
+	public ArrayList<Module> getPredecesseurs(){
+		return this.predecesseurs;
+	}
+
+	public ArrayList<Module> getSuccesseurs(){
+		return this.successeurs;
+	}
+
+	public Etude getEtude(){
+		return this.etude;
+	}
+
+	public void setEtude(Etude etude){
+		this.etude = etude;
+	}
+
+	public ArrayList<String> getProblemes() {
+		return this.problemesDeCoherence;
+	}	
+
+	
+
+}
---
+++ src/presentation/Workflow.java
@@ -0,0 +1,165 @@
+package presentation;
+
+
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Hashtable;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import abstraction.Etude;
+import abstraction.modules.Module;
+
+/**
+ * 
+ * @author Maxime Ansquer
+ *
+ */
+public class Workflow extends JPanel {
+
+	
+	private Etude etude;
+	private Hashtable<String, JButton> lesBoutons;
+	private MainMaximeAnsquer fenetre;
+
+	public Workflow(Etude etude, MainMaximeAnsquer fenetrePrincipale) {
+		this.etude = etude;
+		this.fenetre = fenetrePrincipale;
+		this.lesBoutons = new Hashtable<String, JButton>();		
+		
+		for(final Module m : etude.getLesModules().values()){
+			
+			JButton bouton = new JButton(m.toString());
+			if(!m.estDisponible()){
+				bouton.setEnabled(false);
+			}
+			else{
+				if(m.estCree()){
+					if(!m.estCoherent()){
+						bouton.setBackground(Color.RED);
+					}
+					else{
+						bouton.setBackground(Color.GREEN);
+					}
+				}				
+			}
+
+			lesBoutons.put(m.getNom(), bouton);			
+			final String nomModule = m.getNom();
+
+			bouton.addActionListener(new ActionListener(){
+
+				public void actionPerformed(ActionEvent e) {
+					fenetre.setContenu(nomModule);
+					m.setCree(true);
+				}				
+
+			});
+		}
+
+		this.setLayout(new GridLayout(8,8));
+
+		//1ere ligne
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(lesBoutons.get("EvenementsRedoutes"));
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+
+		//2eme ligne
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(lesBoutons.get("BiensEssentiels"));
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+
+		//3eme ligne
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(lesBoutons.get("MappingDesBiens"));
+		this.add(new JLabel());
+		this.add(lesBoutons.get("AnalyseDesRisques"));
+		this.add(new JLabel());
+		this.add(lesBoutons.get("MatriceDesRisques"));
+
+		//4eme ligne
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(lesBoutons.get("BiensSupports"));
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+
+		//5eme ligne
+		this.add(lesBoutons.get("TypologieDesBiensSupports"));
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+
+		//6eme ligne
+		this.add(new JLabel());
+		this.add(lesBoutons.get("ScenariosDeMenacesGeneriques"));		
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+
+		//7eme ligne
+		this.add(lesBoutons.get("SourcesDeMenaces"));
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(lesBoutons.get("ScenariosDeMenacesTypes"));
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+
+		//8eme ligne
+		this.add(lesBoutons.get("CriteresDeSecurite"));
+		this.add(new JLabel());
+		this.add(lesBoutons.get("Metriques"));
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+		this.add(new JLabel());
+	}
+
+	private JButton moduleManquant() {
+		JButton moduleManquant = new JButton("Module manquant"); //TODO: a enlever pour le rendu final
+		moduleManquant.setEnabled(false); //TODO: a enlever pour le rendu final
+		return moduleManquant;
+	}
+
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.drawImage(new ImageIcon("images/workflow.jpg").getImage(), 0, 0, this.getWidth(), this.getHeight(), null);
+	}
+
+
+}
