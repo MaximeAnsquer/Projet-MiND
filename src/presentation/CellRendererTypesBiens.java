e34a1e361eafbefc4d0207445a0470b223cf03da
Ali <Ali@172.16.127.134>
2016-01-11 02:04:21 +0100

    Corrigé des erreurs sur Java 6

---
+++ src/presentation/CellRendererTypesBiens.java
@@ -0,0 +1,53 @@
+package presentation;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+
+public class CellRendererTypesBiens extends DefaultTableCellRenderer {
+	
+	public CellRendererTypesBiens(){
+		super();
+		this.setHorizontalAlignment( SwingConstants.CENTER );
+	}
+	
+	public Component getTableCellRendererComponent(JTable table, Object value,
+			boolean isSelected, boolean hasFocus, int row, int column) {
+		Component component = super.getTableCellRendererComponent(table, value,
+				isSelected, hasFocus, row, column);
+		
+	
+		
+		if (value.equals("") || value == null) {
+			Color clr = Color.yellow;
+			component.setBackground(clr);
+		} 
+		else if(value instanceof Integer){
+			int numero = (Integer) value;
+			Color clr;
+			
+			
+			switch(numero){
+			case 1: clr = Color.green;
+			break;
+			case 2: clr = Color.yellow;
+			break;
+			case 3: clr = Color.orange;
+			break;
+			case 4: clr = Color.red;
+			break;
+			default: clr = Color.magenta;
+			}
+			component.setBackground(clr);
+		}
+		else {
+			Color clr = new Color(255, 255, 255);
+			component.setBackground(clr);
+		}
+		return component;
+	}
+
+}
--- src/presentation/FenetreScenarioDeMenacesTypes.java
+++ src/presentation/FenetreScenarioDeMenacesTypes.java
@@ -1,16 +1,10 @@
 package presentation;
 
 
-import java.util.ArrayList;
-
-import javafx.scene.control.CheckBox;
-
 import javax.swing.BoxLayout;
 import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -19,7 +13,6 @@ import javax.swing.table.TableColumn;
 
 import controle.ScenariosDeMenacesTypes.ControlJButtonAjoutLigne;
 import controle.ScenariosDeMenacesTypes.ControlJTable;
-import abstraction.autres.SourceDeMenace;
 import abstraction.modules.ScenariosDeMenacesTypes;
 
 public class FenetreScenarioDeMenacesTypes extends JPanel {
@@ -30,7 +23,6 @@ public class FenetreScenarioDeMenacesTypes extends JPanel {
 	private CellRendererEv rendererEv ;
 	private JComboBox comboBoxIntrinseque ;
 	private JComboBox comboBoxReelle ;
-	// private ArrayList<JCheckBox> checkBoxSourcesMenaces ;
 	private JButton ajoutLigne ;
 
 	public FenetreScenarioDeMenacesTypes(ScenariosDeMenacesTypes module) {
