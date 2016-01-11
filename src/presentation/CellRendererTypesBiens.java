31ea853cbe04c2599e0bacdee2f5d159a582c4fa
Ali <Ali@172.17.3.22>
2016-01-11 10:49:25 +0100

    J'ai commencé à faire des couleurs

--- src/presentation/CellRendererTypesBiens.java
+++ src/presentation/CellRendererTypesBiens.java
@@ -26,14 +26,27 @@ public class CellRendererTypesBiens extends DefaultTableCellRenderer {
 			component.setBackground(clr);
 		} 
 		else if (value instanceof String) {
+			Color clr;
 			switch(row){
+			case 0 : clr=new Color(198, 224, 180);
+			break;
+			case 1 : clr=new Color(248, 203, 173);
+			break;
+			case 2 : clr=new Color(255, 230, 153);
+			break;
+			case 3 : clr=new Color(250, 172, 207);
+			break;
+			case 4 : clr=new Color(189, 215, 238);
+			break;
+			case 5 : clr=new Color(172, 185, 202);
+			break;
+			case 6 : clr=new Color(219, 219, 219);
+			break;
 			default: 
-				Random rand = new Random();
-				float r = rand.nextFloat();
-				float g = rand.nextFloat() / 2f;
-				float b = rand.nextFloat() / 2f;
-				component.setBackground(new Color(r,g,b));
+				clr=new Color((255/(row+1))*(row),(255/(row+1))*(row),(255/(row+1))*(row));
+				component.setBackground(clr);
 		}
+		component.setBackground(clr);
 		} else {
 			Color clr = new Color(255, 255, 255);
 			component.setBackground(clr);
