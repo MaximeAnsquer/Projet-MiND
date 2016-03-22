f87a3a7989c42e61c0823321b4f4b7a3525d4d1b
Maxime Ansquer <Maxime Ansquer@192.168.1.13>
2016-03-22 17:23:44 +0100

    suppression de la résolution de MP4 (faut les faire raquer)

--- src/presentation/FenetreMappingDesBiens.java
+++ src/presentation/FenetreMappingDesBiens.java
@@ -29,18 +29,16 @@ import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumnModel;
 
+import presentation.FenetreBiensSupports.ModeleDynamiqueObjet;
 import abstraction.modules.BiensEssentiels;
 import abstraction.modules.BiensSupports;
 import abstraction.modules.MappingDesBiens;
-import abstraction.modules.Module;
 
 public class FenetreMappingDesBiens extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private JTable table;
-	private JScrollPane scrollPane;
 	private MappingDesBiens mappingDesBiens;
 	JComboBox comboBox = new JComboBox();
 	private JFrame petiteFenetre;
@@ -61,12 +59,12 @@ public class FenetreMappingDesBiens extends JPanel {
 		this.mappingDesBiens = mappingDesBiens;
 		this.mappingDesBiens.actualiserMapping();
 		table = new JTable(new ModeleDynamiqueObjet());
-		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);			
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		this.creerPetiteFenetre();
 		comboBox.addItem("");
 		comboBox.addItem("x");
 		comboBox.addItem("o");
-
+		
 		//TODO régler le problème de cellEditor (l'appliquer que pour les bonnes cases (combo column+row))
 		TableColumnModel columnModel = table.getColumnModel();
 		ModeleDynamiqueObjet modele = (ModeleDynamiqueObjet) table.getModel();
@@ -74,9 +72,7 @@ public class FenetreMappingDesBiens extends JPanel {
 			columnModel.getColumn(i).setCellEditor(new DefaultCellEditor(comboBox));
 		}
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		scrollPane = new JScrollPane(table);
-		this.add(scrollPane);
-		this.fixerPremiereColonneEtPouvoirScrollerLesAutres();	
+		this.add(new JScrollPane(table));
 		JPanel jpanel = new JPanel();
 		jpanel.add(boutonAide());
 		this.add(jpanel);
@@ -97,46 +93,20 @@ public class FenetreMappingDesBiens extends JPanel {
 			public void mouseEntered(MouseEvent e) {}
 			public void mouseExited(MouseEvent e) {}			
 		});
-
-		table.setDefaultRenderer(Object.class, new Renderer1());
+		
+		table.setDefaultRenderer(Object.class, new Renderer());
 		table.setFont(new Font("Arial", Font.PLAIN, 15));
 		table.setRowHeight(50);
 		table.setRowHeight(50);
 	}
-
-	private void fixerPremiereColonneEtPouvoirScrollerLesAutres() {
-		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-		resizeColumnWidth(table);
-		BiensEssentiels biensEssentiels = (BiensEssentiels) mappingDesBiens.getEtude().getModule("BiensEssentiels");
-		int nbCategoriesBiensEssentiels = biensEssentiels.getNomColonnesSup().size();
-		FixedColumnTable fct = new FixedColumnTable(nbCategoriesBiensEssentiels + 1, scrollPane);		
-		JTable fixedTable = fct.getFixedTable();
-		fixedTable.setDefaultRenderer(Object.class, new Renderer2());
-		fixedTable.setFont(new Font("Arial", Font.PLAIN, 15));
-		fixedTable.setRowHeight(50);
-		fixedTable.setRowHeight(50);
-	}
-
-	public void resizeColumnWidth(JTable table) {
-		final TableColumnModel columnModel = table.getColumnModel();
-		for (int column = 0; column < table.getColumnCount(); column++) {
-			int width = 50; // Min width
-			for (int row = 0; row < table.getRowCount(); row++) {
-				TableCellRenderer renderer = table.getCellRenderer(row, column);
-				Component comp = table.prepareRenderer(renderer, row, column);
-				width = (int) Math.max(1.5*comp.getMinimumSize().width + 1 , width);
-			}
-			columnModel.getColumn(column).setPreferredWidth(width);
-		}
-	}
-
+	
 	protected void selectionnerLaLigne(MouseEvent e) {
 		Point p = e.getPoint();
 		int rowNumber = table.rowAtPoint(p);
 		int colNumber = table.columnAtPoint(p);
 		table.changeSelection(rowNumber, colNumber, false, true);		
 	}
-
+	
 	protected void setPetiteFenetre() {
 		int row = this.table.getSelectedRow();
 		int col = this.table.getSelectedColumn();
@@ -171,7 +141,7 @@ public class FenetreMappingDesBiens extends JPanel {
 		textAreaPetiteFenetre.setLineWrap(true);
 		textAreaPetiteFenetre.setWrapStyleWord(true);
 	}
-
+	
 	private JButton boutonAide() {
 		JButton bouton = new JButton("Aide");
 		bouton.addActionListener(new ActionListener(){
@@ -275,9 +245,9 @@ public class FenetreMappingDesBiens extends JPanel {
 		}
 	}
 
-	class Renderer1 extends DefaultTableCellRenderer {
+	class Renderer extends DefaultTableCellRenderer {
 
-		public Renderer1() {
+		public Renderer() {
 			super();
 			this.setHorizontalAlignment(SwingConstants.CENTER);
 		}
@@ -287,39 +257,11 @@ public class FenetreMappingDesBiens extends JPanel {
 		public Component getTableCellRendererComponent(JTable table,Object value, boolean isSelected, boolean hasFocus, int row,int column) {
 			Component component = super.getTableCellRendererComponent(table,value, isSelected, hasFocus, row, column);
 			ModeleDynamiqueObjet modele = (ModeleDynamiqueObjet) table.getModel();
-
+			
 			if (component instanceof JComponent) {
 				((JComponent) component).setToolTipText(value.toString());
 			}
-
-			if (row < modele.biensSupports.getNomColonnesSup().size() + 1){
-					component.setBackground(new Color(222, 222, 222));
-			}
-			else{
-				component.setBackground(Color.white);
-			}
-
-			return component;
-		}
-	}
-
-	class Renderer2 extends DefaultTableCellRenderer {
-
-		public Renderer2() {
-			super();
-			this.setHorizontalAlignment(SwingConstants.CENTER);
-		}
-
-		private static final long serialVersionUID = 1L;
-
-		public Component getTableCellRendererComponent(JTable table,Object value, boolean isSelected, boolean hasFocus, int row,int column) {
-			Component component = super.getTableCellRendererComponent(table,value, isSelected, hasFocus, row, column);
-			ModeleDynamiqueObjet modele = (ModeleDynamiqueObjet) table.getModel();
-
-			if (component instanceof JComponent) {
-				((JComponent) component).setToolTipText(value.toString());
-			}
-
+			
 			if (row < modele.biensSupports.getNomColonnesSup().size() +1){
 				if (row == modele.biensSupports.getNomColonnesSup().size()){
 					if (column < modele.biensEssentiels.getNomColonnesSup().size() + 1){
@@ -355,6 +297,4 @@ public class FenetreMappingDesBiens extends JPanel {
 			return component;
 		}
 	}
-
-
 }
